a79371940cac6c26d969c9268ee7f860529b95e5
dinesh-wj <47315121+dinesh-wj@users.noreply.github.com>
2019-03-25 16:02:53 +0530

    Update TestClass.java

--- src/testpack/TestClass.java
+++ src/testpack/TestClass.java
@@ -5,5 +5,6 @@ public class TestClass
     public static void main(String[] args)
     {
         System.out.println("TestClass.main");
+        System.out.println("TestClass.main remote");
     }
 }
